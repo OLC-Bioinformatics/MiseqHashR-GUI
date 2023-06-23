d10951a1ec8bf9f484ca47452e6d695463ecfb43
LargeKowalski888 <LargeKowalski888>
2023-06-23 11:11:07 -0400

    Small merge from main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from PySide2 import QtCore, QtGui, QtWidgets
 from PySide2.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
 from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
 from PySide2.QtWidgets import *
-import openpyxl, csv, subprocess, os, pathlib, psutil, glob
+import subprocess, os, glob
 
 # GUI FILE
 from app_modules import *
